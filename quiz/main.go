a665a525e5d34660699e7a269abbdea3462af556
Zubin Pratap <zubin.pratap@gmail.com>
2022-01-30 11:38:52 +1100

    add timer using go routines and channels

--- quiz/main.go
+++ quiz/main.go
@@ -7,7 +7,10 @@ import (
 	"fmt"
 	"os"
 	"strings"
+	"time"
 )
+
+const TOTAL_TIME = 30
 
 func main() {
 	fmt.Println("Here we go. ")
@@ -27,20 +30,37 @@ func main() {
 	}
 
 	quizItems := parseRecords(records)
-
 	inputReader := bufio.NewReader(os.Stdin)
+	timer := time.NewTimer(TOTAL_TIME * time.Second)
+
 	var score int
 	for i, item := range quizItems {
 		fmt.Printf("\nQuestion #%d is: %s = ?\n", i+1, item.q)
-		answer := getInput(inputReader)
-		if answer != strings.TrimSpace(item.a) {
-			fmt.Printf("Wrong! The answer is %s.\n.", item.a)
-		} else {
-			score += 1
-			fmt.Printf("Correct! Your score is now %d/%d.\n", score, len(quizItems))
+
+		// Spin up goroutine to collect answer and pass it to main routine.
+		ansCh := make(chan string)
+		go func() {
+			input, err := inputReader.ReadString('\n') // blocks until input
+			if err != nil {
+				exit(fmt.Sprintf("error reading user input: %v", err))
+			}
+			ansCh <- strings.TrimSpace(input)
+		}()
+
+		select {
+		case <-timer.C:
+			fmt.Printf("\nTime's up!! You scored %d out of %d.\n", score, len(quizItems))
+			return
+		case answer := <-ansCh:
+			if answer != strings.TrimSpace(item.a) {
+				fmt.Printf("Wrong! The answer is %s.\n.", item.a)
+			} else {
+				score += 1
+				fmt.Printf("Correct! Your score is now %d/%d.\n", score, len(quizItems))
+			}
+			// No default case.  Either timer messages or the goRoutine do
 		}
 	}
-
 	fmt.Printf("Quiz over. Your final score is  %d/%d.\n", score, len(quizItems))
 }
 
@@ -58,16 +78,6 @@ func parseRecords(records [][]string) []QuizItem {
 		}
 	}
 	return items
-}
-
-func getInput(reader *bufio.Reader) string {
-	input, err := reader.ReadString('\n')
-
-	if err != nil {
-		exit(fmt.Sprintf("error reading user input: %v", err))
-	}
-
-	return strings.TrimSpace(input)
 }
 
 func exit(msg string) {
