201e09a6044c0e93c5a622a9962d7d062769d4b6
Zubin Pratap <zubin.pratap@gmail.com>
2022-03-05 08:45:37 +1100

    interim. APIs working. refactoring in progress to factor out http requests

--- captioner/main.go
+++ captioner/main.go
@@ -1,12 +1,15 @@
 package main
 
 import (
-	"bytes"
 	"encoding/json"
 	"fmt"
+	"io"
 	"io/ioutil"
 	"log"
 	"net/http"
+	"os"
+	"path/filepath"
+	"time"
 
 	"github.com/spf13/viper"
 )
@@ -23,69 +26,85 @@ const (
 )
 
 func main() {
-
-	API_KEY := getApiKey("API_KEY")
-	FILE_PATH := "/Users/zubinpratap/Downloads/clip.mp4"
-
-	fileData, err := ioutil.ReadFile(FILE_PATH)
+	FILE_NAME := getFilePathFromUser()
+	fileData, err := ioutil.ReadFile(FILE_NAME)
 	if err != nil {
 		log.Fatalln("error loading file to be transcribed: ", err)
 	}
 
 	// Setup HTTP client and set header
 	client := &http.Client{}
-	req, err := http.NewRequest("POST", UPLOAD_ENDPOINT, bytes.NewBuffer(fileData))
-	if err != nil {
-		log.Fatalln("error constructing new request to upload endpoint: ", err)
-	}
-	req.Header.Set("authorization", API_KEY)
+	var responses map[string]interface{}
 
-	// Make the HTTP request.
-	res, err := client.Do(req)
-	if err != nil {
-		log.Fatalln("error making http request to the upload endpoint: ", err)
+	// Make HTTP request to upload file.
+	// res := requestEndpoint(client, "POST", UPLOAD_ENDPOINT, false, bytes.NewBuffer(fileData))
+	// updateResponses(*res, &responses)
+
+	// fileUrl := responses["upload_url"]
+	// fileEndpoint, ok := fileUrl.(string)
+	// if !ok {
+	// 	log.Fatalln("file endpoint is not of type string")
+	// }
+	// fmt.Println("File is uploaded to ", fileEndpoint)
+
+	// // Construct JSON object to send to transcription API.
+	// sendData := map[string]string{"audio_url": fileEndpoint}
+	// sendDataJson, err := json.Marshal(sendData)
+	// if err != nil {
+	// 	log.Fatalln("error marshalling data to be sent into json: ", err)
+	// }
+
+	// Make HTTP request to initiate transcription and get id.
+	// res = requestEndpoint(client, "POST", TRANSCRIPT_ENDPOINT, true, bytes.NewBuffer(sendDataJson))
+	// updateResponses(*res, &responses)
+
+	// id := responses["id"]
+	// idString, ok := id.(string)
+	// if !ok {
+	// 	log.Fatalln("transcription id is not a string")
+	// }
+	// fmt.Println("Transcription succeeded - id is: ", id)
+
+	// Make HTTP request to get transcription text.
+	STATUS_POLLING_URL := TRANSCRIPT_ENDPOINT + "/" + "ogfuflk54l-ec9f-48b6-b9c6-b8278e23eeac" // idString  TODO reinstate
+	_ = fileData                                                                               // TODO remove
+
+	done := false
+	for !done {
+		res := requestEndpoint(client, "GET", STATUS_POLLING_URL, false, nil)
+
+		status, ok := responses["status"].(string)
+		if !ok {
+			fmt.Printf("Status field value is: %v\n", responses["status"])
+		}
+
+		fmt.Printf("ZUBIN the res status is : %v\n", status)
+
+		status = "completed" // TODO reemove
+
+		if status == "completed" {
+			done = true
+			parseJsonResp(*res, &responses)
+			break
+		}
+		// Processing not complete.
+		fmt.Println("Transcription not complete. Trying again in 1 minute...")
+		time.Sleep(1 * time.Second)
+		// time.Sleep(1 * time.Minute) // TODO reinstate
 	}
+
+	// Get the SRT text.
+	SRT_ENDPOINT := TRANSCRIPT_ENDPOINT + "/" + "ogfuflk54l-ec9f-48b6-b9c6-b8278e23eeac" + "/" + "srt" //   TODO replace with idString
+	res := requestEndpoint(client, "GET", SRT_ENDPOINT, false, nil)
 	defer res.Body.Close()
-
-	// Decode JSON response into a map.
-	var result map[string]interface{} // result is expected to be a map.
-	if err := json.NewDecoder(res.Body).Decode(&result); err != nil {
-		log.Fatalln("error decoding result to JSON: ", err)
-	}
-
-	fmt.Println(" The file is uploaded and available at: ", result)
-	fileUrl := result["upload_url"]
-	fileEndpoint, ok := fileUrl.(string)
-	if !ok {
-		log.Fatalln("file endpoint is not of type string")
-	}
-
-	// Construct JSON object to send to transcription API.
-	sendData := map[string]string{"audio_url": fileEndpoint}
-	sendDataJson, err := json.Marshal(sendData)
+	body, err := io.ReadAll(res.Body)
 	if err != nil {
-		log.Fatalln("error marshalling data to be sent into json: ", err)
+		log.Fatalf("error reading SRT text from response: ", err)
 	}
 
-	// Make HTTP request.
-	req, err = http.NewRequest("POST", TRANSCRIPT_ENDPOINT, bytes.NewBuffer(sendDataJson))
-	if err != nil {
-		log.Fatalln("error constructing new request to  transcription endpoint: ", err)
-	}
-	req.Header.Set("content-type", "application/json")
-	req.Header.Set("authorization", API_KEY)
+	fmt.Println("Here is the transcribed text: ", string(body))
 
-	res, err = client.Do(req)
-	if err != nil {
-		log.Fatalln("error making http request to the transcription endpoint: ", err)
-	}
-	defer res.Body.Close()
-
-	var transcribeResult map[string]interface{}
-	json.NewDecoder(res.Body).Decode(&transcribeResult)
-
-	id := transcribeResult["id"]
-	fmt.Println("Id is ", id)
+	writeSrtFile(FILE_NAME, string(body))
 
 }
 
@@ -95,4 +114,76 @@ func getApiKey(key string) string {
 		log.Fatal("Env Variable not of type string")
 	}
 	return env
+}
+
+func prettyPrintMap(m map[string]interface{}) string {
+	s, err := json.MarshalIndent(m, "", "  ")
+	if err != nil {
+		fmt.Println("error marshalling map to indented json:", err)
+	}
+	return string(s)
+}
+
+func getFilePathFromUser() string {
+	return "/Users/zubinpratap/Downloads/clip.mp4"
+}
+
+func requestEndpoint(client *http.Client, method, url string, contentTypeJson bool, body io.Reader) *http.Response {
+	fmt.Printf("making %s request to %s\n", method, url)
+	API_KEY := getApiKey("API_KEY")
+
+	// Construct request to the endpoint.
+	req, err := http.NewRequest(method, url, body)
+	if err != nil {
+		log.Fatalln("error constructing new request to  transcription endpoint: ", err)
+	}
+
+	// Set headers.
+	req.Header.Set("authorization", API_KEY)
+	if contentTypeJson {
+		req.Header.Set("content-type", "application/json")
+	}
+
+	// Make request.
+	res, err := client.Do(req)
+	if err != nil {
+		log.Fatalln("error making http request to the transcription endpoint: ", err)
+	}
+
+	if res.StatusCode != http.StatusOK {
+		fmt.Printf("Request status code not OK - %v", res.StatusCode)
+	}
+
+	return res
+}
+
+// Takes the given http responses and decodes it's JSON into the the responses map pointer.
+func parseJsonResp(res http.Response, responses *map[string]interface{}) {
+	if err := json.NewDecoder(res.Body).Decode(responses); err != nil {
+		log.Fatalln("error decoding result to JSON: ", err)
+	}
+
+	resps := *responses
+	if resps["error"] != nil {
+		log.Fatalln("error string found in responses: ", resps["error"])
+	}
+
+	fmt.Printf("Just updated responses with %v", prettyPrintMap(*responses))
+}
+
+func writeSrtFile(transcribedFilePath, srtData string) {
+	dir, sourceFile := filepath.Split(transcribedFilePath)
+	fmt.Printf("REMOVE ME:  %s", dir+sourceFile+".srt") // TODO remove
+
+	f, err := os.Create(dir + sourceFile + ".srt")
+	if err != nil {
+		log.Fatalln("error creating srt file: ", err)
+	}
+	defer f.Close()
+
+	_, err2 := f.WriteString(srtData)
+
+	if err2 != nil {
+		log.Fatalln("error writing srt file: ", err2)
+	}
 }
